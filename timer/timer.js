86db054a9870f8e9c3e606243d7ef7a06c8a4b20
Keith Levi <guilddrive@gmail.com>
2021-03-13 08:57:06 -0600

    Update timer.js

--- timer/timer.js
+++ timer/timer.js
@@ -4,16 +4,33 @@
 /* eslint-disable */
 
 
-function pageLoad() { document.getElementById("go").onclick = goClick; } 
+function pageLoad() { document.getElementById("go").onclick = goClick; }
 
-function goClick() { 
-    const seconds = +document.getElementById("sec").value;
-    setTimeout(alarm, seconds * 1000);
-} 
+function goClick() {
+    //const seconds = +document.getElementById("sec").value;
+    setTimeout(countDown, 1000);
+    //setInterval(countDown, 1000);
+}
 
-function alarm(){
+function alarm() {
     const bodyElement = document.getElementById("body");
     bodyElement.className = "awesome";
+}
+
+/**
+ * make the textbox count down the seconds
+ * get the current value and reduce it to 0
+ * if value is 0 cancel the timer
+ */
+function countDown() {
+    secondsTextbox = document.getElementById("sec")
+    const seconds = +secondsTextbox.value;
+    if (seconds > 0) {
+        secondsTextbox.value = seconds - 1;
+        setTimeout(countDown, 1000);
+    } else {
+        alarm();
+    }
 }
 
 window.onload = pageLoad;
